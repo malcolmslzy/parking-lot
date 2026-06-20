450a77970fb891592492cb650b671b98ad65f562
kail-hyyuzz <norisrm550@gmail.com>
2026-06-20 17:01:25 +0800

    Update parking.h

--- parking.h
+++ parking.h
@@ -23,8 +23,11 @@ bool isFull();
 void addCar();
 void displayLot();
 
-/* Member 1 function
-xxxxxxx */
+void enqueue(Queue& q, string regNumber, string entryDate, string entryTime);
+void dequeue(Queue& q);
+void searchByCar(string searchPlate);
+void checkSlot(int slotNumber);
+void displayReport();
 
 /* member 3 fucntion
 xxxxxxxxxx */
